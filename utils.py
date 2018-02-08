358784d02a09ab24db8a0f3b9ebba2a441d7ced8
Charles Vandermies <you@example.com>
2018-02-08 11:10:45 +0100

    Add Travis configuration

--- utils.py
+++ utils.py
@@ -2,36 +2,30 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
+from collections import namedtuple
 
 def fact(n):
-    """Computes the factorial of a natural number.
-    
-    Pre: -
-    Post: Returns the factorial of 'n'.
-    Throws: ValueError if n < 0
-    """
+    if n<0:
+        raise ValueError
+    if n<2:
+        return 1
+    else :
+        return n*fact(n-1)
 
-def roots(a, b, c):
-    """Computes the roots of the ax^2 + bx + x = 0 polynomial.
-    
-    Pre: -
-    Post: Returns a tuple with zero, one or two elements corresponding
-          to the roots of the ax^2 + bx + c polynomial.
-    """
-    pass
 
-def integrate(function, lower, upper):
-    """Approximates the integral of a fonction between two bounds
-    
-    Pre: 'function' is a valid Python expression with x as a variable,
-         'lower' <= 'upper',
-         'function' continuous and integrable between 'lower‘ and 'upper'.
-    Post: Returns an approximation of the integral from 'lower' to 'upper'
-          of the specified 'function'.
-    """
-    pass
+def roots(a,b,c):
+    if a==0:
+        print("Veuillez spécifier une équation du second degré")
+    else:
+        delta = (b**2)-(4*a*c)
+        if delta==0:
+            return (-b/(2*a))
+        elif delta > 0:
+            x1 = (-b + delta**0.5)/(2*a)
+            x2 = (-b - delta**0.5)/(2*a)
+            return (x1,x2)    
+        else :
+            return ('Impossible')
 
-if __name__ == '__main__':
-    print(fact(5))
-    print(roots(1, 0, 1))
-    print(integrate('x ** 2 - 1', -1, 1))
+print(roots(1,4,3))
+            
